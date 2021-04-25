eea5d41f443b1a3824cbfbe39e19dc74301ff810
Miklos Ferencz <miklos.ferencz@gmail.com>
2021-04-25 21:12:12 +0200

    password test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # config.action_mailer.default_url_options = { host: 'localhost' }
+
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -59,4 +62,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+
+# Add this line next to existing config.action_mailer settings
+  config.action_mailer.delivery_method = :smtp #:letter_opener
 end
